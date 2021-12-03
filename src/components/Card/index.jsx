d580637c9a4450426acc65c1bd95467f2e919620
amirrezamahmoudzadeh <81969097+amirrezamahmoudzadeh@users.noreply.github.com>
2021-12-03 22:34:38 +0330

    Update index.jsx

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,15 +1,30 @@
-import React from "react";
-import "./Card.css";
+import React, {useState } from "react";
+import Card from "./components/Card";
+import Cards from "./data/Cards";
 
-function Card({ id, select, onChangeColor }) {
+
+function App() {
+  
+  const [cards, setCards] = useState(Cards)
+
+  const selectCardHandler = (id)=>{
+    let cardId = cards.findIndex((card => card.id === id))
+    let selectedCard = {...cards[cardId]}
+    selectedCard.selected= !selectedCard.selected
+
+    let updatedCards = [...cards]
+    updatedCards[cardId]=selectedCard
+    setCards(updatedCards)
+
+  }
+
   return (
-    <div
-      className="card"
-      id={id}
-      onClick={() => onChangeColor(id)}
-      style={{ backgroundColor: select ? "pink" : "skyblue" }}
-    ></div>
+    <div className="container">
+      {cards.map((card)=>{
+        return <Card id={card.id} key={card.id} select={card.selected} onChangeColor={selectCardHandler} />
+      })}
+    </div>
   );
 }
 
-export default Card;
+export default App;
